d3a41d9d1954f24fbf4b8e7a0ee6725ac268a64b
Dhananjay kumar <107258319+dhananjaykr306@users.noreply.github.com>
2024-11-20 16:52:35 +0530

    [Dhananjay] add :Python Program for Prime no. Checker

---
+++ Logical_program/3_Prime_no.py
@@ -0,0 +1,45 @@
+"""
+    @Author: Dhananjay Kumar
+    @Date: 20-11-2024
+    @Last Modified by: Dhananjay Kumar
+    @Last Modified time: 20-11-2024
+    @Title: Prime Number Checker
+"""
+
+def is_prime_number(n):
+    """
+    Description:
+        This function checks if a given number is a prime number.
+        
+    Parameters:
+        n (int): The number to check if it is prime.
+        
+    Returns:
+        bool: True if the number is prime, False otherwise.
+    """
+    if n <= 1:
+        return False 
+    if n == 2:
+        return True   
+    if n % 2 == 0:
+        return False 
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False  
+    return True 
+
+def main():
+    try:
+        n = int(input("Enter a positive integer to check if it's a Prime Number: "))
+        if n <= 0:
+            print("Please enter a positive integer.")
+        else:
+            if is_prime_number(n):
+                print(f"{n} is a Prime Number.")
+            else:
+                print(f"{n} is not a Prime Number.")
+    except ValueError:
+        print("Invalid input! Please enter a valid integer.")
+
+if __name__ == "__main__":
+    main()
